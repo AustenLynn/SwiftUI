8fa32e10f490bc060d88a3133fc8ea7934e9a9a6
AustenLynn <mikasalvarez@gmail.com>
2024-09-20 17:19:11 -0600

    propinas

--- ola/ContentView.swift
+++ ola/ContentView.swift
@@ -8,30 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var monto = "1000"
+    @State var porcentaje = 10.0
+    
+    
     var body: some View {
         VStack {
             HStack{
-                Image(systemName: "bolt.heart.fill")
+                Image(systemName: "dollarsign")
                     .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hola Mundo en SwiftUI")
-                    .foregroundColor(.pink)
+                    .foregroundStyle(.yellow)
                 
-                Image(systemName: "pencil")
+                Text("Calculadora de propinas")
+                Image(systemName: "dollarsign.circle.fill")
                     .imageScale(.large)
-                    .foregroundColor(.red)
+                    .foregroundColor(.green)
             }
-            .padding()
-            Text("Hola")
-                .padding()
-            Text("Soy")
-                .padding()
-            Text("Maikol")
-                .padding()
-            Image(systemName: "heart.square.fill")
-                .foregroundColor(.red)
+            
+            HStack{
+             Text("$")
+            TextField("Monto", text: $monto)
+            
+            }
+            HStack{
+                Slider(value: $porcentaje, in:  5...100, step:5)
+                Text(String(porcentaje))
+            }
+            
+            HStack{
+                Text("Propina:")
+                if let total = Double(monto){
+                    let propina = total * porcentaje / 100
+                    let monto_total = (Double(monto) ?? 0)  + (propina)
+                    Text(String(propina))
+                    Text("Total" + String(monto_total) )
+                }else{
+                    Text("Debes ingresar el monto en moneda")
+                        .foregroundColor(.red)
+                }
+                
+            }
+            Spacer()
             
         }
+        .padding()
     }
 }
 #Preview {
