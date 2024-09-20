2bcaceda3d1bf801409f87f02a58b987db83cd48
AustenLynn <mikasalvarez@gmail.com>
2024-09-20 16:39:26 -0600

    Initial Commit

---
+++ ola/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ola
+//
+//  Created by D0501 on 20/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ola/olaApp.swift
@@ -0,0 +1,17 @@
+//
+//  olaApp.swift
+//  ola
+//
+//  Created by D0501 on 20/09/24.
+//
+
+import SwiftUI
+
+@main
+struct olaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
